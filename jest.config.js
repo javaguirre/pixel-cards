bd752c8a7d2f5f44373237010c82f6c773e1fa93
Javier Aguirre <contacto@javaguirre.net>
2021-12-28 17:01:56 +0100

    added services to jest module names

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
 
     '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@/services/(.*)$': '<rootDir>/src/services/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 }
